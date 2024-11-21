f3049ed9e68b50769a7871f5c3f0d3b581655fbc
guptadhairy <21it3032@rgipt.ac.in>
2024-11-22 01:35:28 +0530

    debugg

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://jobfair360-backend.onrender.com/api'
+  baseURL: 'https://jobfair360-backend.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
